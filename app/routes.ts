e20f03c1175da251d57444a3e6e33e0fa0957d5d
Niihara Megumu <nhrmgm1223@gmail.com>
2025-04-06 11:42:07 +0900

    login route追加

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, prefix, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    route("/", "routes/top.tsx"),
+    route("/home", "routes/home.tsx"),
+
+    // ログイン・ログアウト
+    ...prefix("auth", [
+        route("/login", "routes/auth/login.tsx")
+    ]),
+] satisfies RouteConfig;
