db9aecd555480cf75b2856519b0777f6c4dceb64
Niihara Megumu <nhrmgm1223@gmail.com>
2025-04-06 19:48:24 +0900

    format

--- app/routes.ts
+++ app/routes.ts
@@ -1,11 +1,9 @@
 import { type RouteConfig, prefix, route } from "@react-router/dev/routes";
 
 export default [
-    route("/", "routes/top.tsx"),
-    route("/home", "routes/home.tsx"),
+	route("/", "routes/top.tsx"),
+	route("/home", "routes/home.tsx"),
 
-    // ログイン・ログアウト
-    ...prefix("auth", [
-        route("/login", "routes/auth/login.tsx")
-    ]),
+	// ログイン・ログアウト
+	...prefix("auth", [route("/login", "routes/auth/login.tsx")]),
 ] satisfies RouteConfig;
