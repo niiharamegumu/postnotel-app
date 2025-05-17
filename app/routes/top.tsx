a167328961908d14751fd8f21630da2e51f40ba2
Niihara Megumu <nhrmgm1223@gmail.com>
2025-05-17 14:05:14 +0900

    top 不要なコンテンツ削除

--- app/routes/top.tsx
+++ app/routes/top.tsx
@@ -1,28 +1,7 @@
-import { useOutletContext } from "react-router";
-import type { UserInfo } from "~/types/user";
-
 export function meta() {
 	return [{ title: "PostNotel" }, { name: "description", content: "PostNotel Top" }];
 }
 
 export default function Top() {
-	const userInfo = useOutletContext<UserInfo>();
-	return (
-		<section>
-			<h1>PostNotel</h1>
-			{userInfo?.name && (
-				<div className="text-xl mb-4">
-					<p>name：{userInfo.name}</p>
-					<p>email：{userInfo.email}</p>
-					<img
-						src={userInfo.avatarUrl}
-						alt="User Profile"
-						className="rounded-full w-[50px] h-[50px] object-cover"
-						loading="lazy"
-						decoding="async"
-					/>
-				</div>
-			)}
-		</section>
-	);
+	return <section>ここにコンテンツが入ります</section>;
 }
