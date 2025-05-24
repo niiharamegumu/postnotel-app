bf389d575641ab934dafdd824cd2a95fe50f6f3f
Niihara Megumu <nhrmgm1223@gmail.com>
2025-05-24 18:40:56 +0900

    create, updateを切り替える処理追加

--- app/layout/withPost.tsx
+++ app/layout/withPost.tsx
@@ -1,11 +1,12 @@
 import { format } from "date-fns";
 import { Plus } from "lucide-react";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useCallback, useMemo, useState } from "react";
 import { Outlet, useNavigate, useOutletContext, useSearchParams } from "react-router";
 import { toast } from "sonner";
+import { ApiResponseError } from "~/api/error/apiResponseError";
 import FloatMenu from "~/components/common/floatMenu";
 import { Button } from "~/components/ui/button";
-import type { AccessLevel } from "~/constants/accessLevel";
+import type { Note, NoteApiRequest } from "~/features/notes/types/note";
 import type { UserInfo } from "~/types/user";
 
 const BlockNoteDrawer = lazy(() => import("~/components/common/.client/blockNoteDrawer"));
@@ -15,10 +16,12 @@ export default function WithPost() {
 	const navigate = useNavigate();
 	const [searchParams] = useSearchParams();
 
-	const createNote = async (params: {
-		content: string;
-		accessLevel: AccessLevel;
-	}): Promise<void> => {
+	const [loading, setLoading] = useState(false); // 処理中かどうか
+	const [isNoteDrawerOpen, setNoteDrawerOpen] = useState(false); // noteのドロワーの開閉状態
+	const [noteDrawerType, setNoteDrawerType] = useState<"create" | "edit">("create"); // noteのドロワーのタイプ
+	const [targetNote, setTargetNote] = useState<Note | null>(null);
+
+	const createNote = async (params: NoteApiRequest): Promise<void> => {
 		const { content, accessLevel } = params;
 		if (!content) return;
 
@@ -43,10 +46,63 @@ export default function WithPost() {
 		toast.success("ノートを作成しました");
 	};
 
+	const editNote = async (params: NoteApiRequest): Promise<void> => {
+		const { content, accessLevel } = params;
+		if (!content) return;
+		if (!targetNote) return;
+
+		const date = searchParams.get("date");
+		const targetDate = date ? new Date(date) : new Date();
+		const body = JSON.stringify({
+			content,
+			accessLevel,
+		});
+
+		const res = await fetch(`/notes/${targetNote.noteId}/update`, {
+			method: "POST",
+			body,
+		});
+
+		if (!res.ok) {
+			throw new ApiResponseError(res.status, "ノートの編集に失敗しました");
+		}
+
+		navigate(`/notes?date=${format(targetDate, "yyyy-MM-dd")}`);
+		toast.success("ノートを編集しました");
+	};
+
+	let noteDrawerHandler = createNote;
+	switch (noteDrawerType) {
+		case "create":
+			noteDrawerHandler = createNote;
+			break;
+		case "edit":
+			noteDrawerHandler = editNote;
+			break;
+	}
+
+	const onClickEditNote = useCallback(
+		(note: Note) => {
+			if (!userInfo) return;
+			setTargetNote(note);
+			setNoteDrawerType("edit");
+			setNoteDrawerOpen(true);
+		},
+		[userInfo],
+	);
+
+	const contextValue = useMemo(
+		() => ({
+			userInfo,
+			onClickEditNote,
+		}),
+		[userInfo, onClickEditNote],
+	);
+
 	return (
 		<div className="flex flex-col min-h-screen">
 			<main className="max-w-full pt-6 pb-25 px-6">
-				<Outlet context={userInfo} />
+				<Outlet context={contextValue} />
 				<div className="fixed bottom-10 left-0 right-0 z-50 flex justify-center gap-2">
 					<FloatMenu userInfo={userInfo} />
 					<Suspense
@@ -57,7 +113,16 @@ export default function WithPost() {
 						}
 					>
 						{userInfo && (
-							<BlockNoteDrawer onSubmit={createNote} type="create" buttonLabel="Create Note" />
+							<BlockNoteDrawer
+								onSubmit={noteDrawerHandler}
+								noteDrawerType={noteDrawerType}
+								setNoteDrawerType={setNoteDrawerType}
+								loading={loading}
+								setLoading={setLoading}
+								open={isNoteDrawerOpen}
+								setOpen={setNoteDrawerOpen}
+								note={targetNote}
+							/>
 						)}
 					</Suspense>
 				</div>
