5e8b7776128caadcfd987c1d892ad1f6333a5cd2
Niihara Megumu <nhrmgm1223@gmail.com>
2025-06-23 19:27:41 +0900

    Wines画面実装

--- app/routes/wines/index.tsx
+++ app/routes/wines/index.tsx
@@ -1,3 +1,84 @@
+import { fetchNotes } from "~/features/notes/api/get";
+import type { Route } from "./+types";
+import { lazy, Suspense } from "react";
+import { useLoaderData } from "react-router";
+import type { NotesByDateResponse } from "~/features/notes/types/note";
+import { format } from "date-fns";
+import { Skeleton } from "~/components/ui/skeleton";
+import { AccessLevel } from "~/constants/accessLevel";
+import { NoteContentType } from "~/constants/noteContentType";
+
+export async function loader({ request, context }: Route.LoaderArgs) {
+	const targetContentType = NoteContentType.WineByAi; // ワインノートを取得するためのコンテンツタイプ
+	// TODO： 将来的には10件程度でページネーションを実装する
+	const notes = await fetchNotes(request, context, { contentType: targetContentType });
+	return { notes };
+}
+
+const NoteContent = lazy(() => import("~/features/notes/components/.client/content"));
+
 export default function Index() {
-	return <div>Wines</div>;
+	const { notes } = useLoaderData<typeof loader>() as {
+		notes: NotesByDateResponse | null;
+	};
+
+	return (
+		<div className="max-w-2xl mx-auto py-8 space-y-10">
+			<section>
+				<h1 className="text-3xl font-bold">
+					Wines <span className="text-muted-foreground text-sm">By AI</span>
+				</h1>
+			</section>
+
+			<section>
+				{notes && notes.notes.length > 0 ? (
+					<ul className="space-y-8">
+						{notes.notes.map((note) => (
+							<Suspense
+								key={note.noteId}
+								fallback={
+									<li>
+										<Skeleton className="h-10 w-full" />
+									</li>
+								}
+							>
+								<li className="flex flex-col items-start">
+									<h2 className="text-xl font-semibold mb-2">
+										{format(new Date(note.createdAt), "yyyy年MM月dd日 HH時mm分")}
+									</h2>
+									{note.images?.length > 0 && (
+										<div className="mb-3">
+											<div className="flex gap-2 flex-wrap">
+												{note.images.map((img, i) => (
+													<div
+														key={`${note.noteId}-img-${i}`}
+														className={`rounded-xl overflow-hidden p-2 ${
+															note.accessLevel === AccessLevel.Private
+																? "bg-secondary"
+																: "bg-primary"
+														}`}
+													>
+														<img
+															src={img}
+															alt={`ワイン画像 #${i + 1}`}
+															className="w-48 h-auto object-cover rounded-xl"
+														/>
+													</div>
+												))}
+											</div>
+										</div>
+									)}
+									<div className="wrap-anywhere max-h-[500px] overflow-y-auto rounded-xl mb-1">
+										<NoteContent note={note} />
+									</div>
+								</li>
+							</Suspense>
+						))}
+					</ul>
+				) : (
+					<p className="text-primary text-center mt-10">ワインノートがありません。</p>
+				)}
+			</section>
+		</div>
+	);
 }
