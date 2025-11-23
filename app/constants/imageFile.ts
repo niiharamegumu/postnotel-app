29b3c9b03f6535421b7844fde0a13470c7a34903
Niihara Megumu <nhrmgm1223@gmail.com>
2025-11-23 18:30:50 +0900

    feat: Increase image compression maximum dimension and file size limits.

--- app/constants/imageFile.ts
+++ app/constants/imageFile.ts
@@ -13,10 +13,10 @@ export const allowedImageContentTypes: string[] = Object.values(AllowImageConten
 export const allowedImageFileExtensions: string[] = Object.values(AllowImageFileExtension);
 
 // Ensures resized images remain suitable for desktop and mobile screens.
-export const imageCompressionMaxDimension = 1920;
+export const imageCompressionMaxDimension = 2500;
 
 export const imageCompressionOptions = {
-	maxSizeMB: 0.2,
+	maxSizeMB: 0.4,
 	maxWidthOrHeight: imageCompressionMaxDimension,
 	fileType: "image/webp",
 	useWebWorker: true,
